e291685c08386c9b9b81bb044317e8796deee499
ryancurson20 <ryan@broadlanddigital.co.uk>
2024-10-25 17:00:38 +0100

    add project

---
+++ detailor/src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  os: string = 'Other';
+  browser: string = 'Other';
+  res: string = 'Other';
+  ip: string = 'Other';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.getOS();
+    this.getBrowser();
+    this.getScreenRes();
+    this.getIP();
+  }
+
+  getOS() {
+    this.os = navigator.userAgent.slice(13).split(';')[0]
+  }
+
+  getBrowser() {
+    var ua = navigator.userAgent;
+    console.log(ua)
+    console.log(navigator)
+
+    if(ua.indexOf("Opera")!=-1) {
+      this.browser="Opera";
+    }
+    if(ua.indexOf("Firefox")!=-1 && ua.indexOf("Opera")==-1) {
+        this.browser="Firefox";
+        // Opera may also contains Firefox
+    }
+    if(ua.indexOf("Chrome")!=-1) {
+        this.browser="Google Chrome";
+    }
+    if(ua.indexOf("Safari")!=-1 && ua.indexOf("Chrome")==-1) {
+        this.browser="Safari";
+        // Chrome always contains Safari
+    }
+    
+    if(ua.indexOf("MSIE")!=-1 && (ua.indexOf("Opera")==-1 && ua.indexOf("Trident")==-1)) {
+        this.browser="Internet Explorer";
+        //user agent with MSIE and Opera or MSIE and Trident may exist.
+    }
+    
+    if(ua.indexOf("Trident")!=-1) {
+        this.browser="Internet Explorer";
+    }
+  }
+
+  getScreenRes() {
+    this.res = window.screen.width + ' x ' + window.screen.height;
+  }
+
+  getIP() {
+    this.http.get('http://www.geoplugin.net/json.gp').subscribe((res:any) => {
+      this.ip = res['geoplugin_request']
+    })
+  }
+  
+}
+
